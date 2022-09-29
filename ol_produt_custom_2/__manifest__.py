db2cae3db4a72cb83168a88db1e12da12878cf6f
Huzaifa Ahmed <44926606+huxe@users.noreply.github.com>
2022-09-29 13:35:21 +0500

    product fields

---
+++ ol_produt_custom_2/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Part of Softhealer Technologies.
+{
+    "name": "Ol Product Custom",
+
+    "author": "",
+
+    "license": "OPL-1",
+
+    "version": "15.0.1",
+
+    "depends": [
+        'sale_management',
+    ],
+
+    "data": [
+        'security/ir.model.access.csv',
+        'views/ol_pdc.xml',
+        'reports/report.xml',
+        # 'reports/ali_sale_agreement.xml',
+        'reports/new_report.xml',
+
+    ],
+    "images": [ ],
+    "auto_install": False,
+    "application": True,
+    "installable": True,
+    "price": "60",
+    "currency": "EUR",
+}
---
+++ ol_produt_custom_2/models/main.py
@@ -0,0 +1,91 @@
+from odoo import models, fields,api
+from odoo.exceptions import UserError
+import base64
+import requests
+import datetime
+
+class accountmove(models.Model):
+    _inherit = 'product.product'
+
+
+    brand_id = fields.Many2one('brand.main.custom',  string="Brand")
+    grp_id = fields.Many2one('brand.custom',  string="Group")
+    sub_grp_id = fields.Many2one('sub.group.custom',  string="Sub Group")
+    sub_sub_grp_id = fields.Many2one('sub.sub.group.custom',  string="Sub Sub Group")
+
+    @api.onchange('grp_id')
+    def set_domain_sub_grp(self):
+
+
+        # self.sub_grp_id = False
+        if self.grp_id:
+            return {'domain': {'sub_grp_id': [('grp_id', '=', self.grp_id.id)]}}
+
+        else:
+            # remove the domain if no contrat is selected
+            return {'domain': {'sub_grp_id': []}}
+
+    @api.onchange('sub_grp_id')
+    def set_domain_sub_sub_grp(self):
+
+        # self.sub_grp_id = False
+        if self.sub_grp_id:
+            return {'domain': {'sub_sub_grp_id': [('sub_grp_id', '=', self.sub_grp_id.id)]}}
+
+        else:
+            # remove the domain if no contrat is selected
+            return {'domain': {'sub_sub_grp_id': []}}
+
+
+
+
+class Brandsclass(models.Model):
+    _name ='brand.custom'
+
+
+    name = fields.Char('Name',required=True)
+    code = fields.Char('Code')
+    discription = fields.Char('Discription')
+
+
+
+
+
+
+
+
+
+
+class SubGroupClass(models.Model):
+    _name ='sub.group.custom'
+
+
+    name = fields.Char('Name',required=True)
+    code = fields.Char('Code')
+    discription = fields.Char('Discription')
+    grp_id = fields.Many2one('brand.custom',string="Group")
+
+
+
+
+
+class SubSubGroupClass(models.Model):
+    _name ='sub.sub.group.custom'
+
+
+    name = fields.Char('Name',required=True)
+    code = fields.Char('Code')
+    discription = fields.Char('Discription')
+    sub_grp_id = fields.Many2one('sub.group.custom',string="Sub Group")
+    # brand_main_ids = fields.Many2one('brand.main.custom', string="Brand ID")
+
+
+
+
+class BRandClass(models.Model):
+    _name ='brand.main.custom'
+
+
+    name = fields.Char('Name',required=True)
+    code = fields.Char('Code')
+    discription = fields.Char('Discription')
