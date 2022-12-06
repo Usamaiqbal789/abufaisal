60d3a3b57b93394790e7f3934da5ab7ea4499c41
arafay020 <a.rafay@odolution.com>
2022-12-06 17:46:48 +0500

    update dashboard

--- ol_produt_custom_2/models/product_dashboard.py
+++ ol_produt_custom_2/models/product_dashboard.py
@@ -74,6 +74,7 @@ class CustomDashboard(models.TransientModel):
     _rec_name = 'product_db_id'
 
     product_db_id = fields.Many2one('product.product', string="Part Number")
+    ir_mod_id = fields.Many2one('ir.module.module', string="Model")
     product_image = fields.Binary('Image',related='product_db_id.image_1920')
     # product_image = fields.Binary('Image')
 
@@ -81,8 +82,8 @@ class CustomDashboard(models.TransientModel):
     own_ref_no = fields.Char(string="Own Ref No", related='product_db_id.own_ref_no')
     part_no = fields.Char(string="Part No", related='product_db_id.default_code')
     product_brand = fields.Many2one(string="Product Brand", related='product_db_id.brand_id')
-    db_detailed_type = fields.Selection(related="product_db_id.type", string='Stock Type')
-    # db_detailed_type = fields.Selection(related="product_db_id.detailed_type", string='Stock Type')
+    # db_detailed_type = fields.Selection(related="product_db_id.type", string='Stock Type')
+    db_detailed_type = fields.Selection(related="product_db_id.detailed_type", string='Stock Type')
     product_description= fields.Text('Product Details',related="product_db_id.description_sale")
     # product_description = fields.Html('Product Details', related="product_db_id.description")
     db_origin = fields.Many2one('res.country',related='product_db_id.origin', string='Origin')
@@ -102,6 +103,8 @@ class CustomDashboard(models.TransientModel):
     logs_price = fields.Char('Price')
     logs_partner_id = fields.Many2one('res.partner', string='Customer')
     dashboard_logs_ids = fields.Many2many('dasboard.logs', string='Dashboard Logs')
+
+
 
     @api.depends('product_db_id')
     def add_log_ids_dash(self):
@@ -144,23 +147,13 @@ class CustomDashboard(models.TransientModel):
                 for q in l.quant_ids:
                     if q.product_id == self.product_db_id:
                         quantities+= q.available_quantity
-                # inventory=self.env['stock.quant'].search([('location_id','=',l.id)])
-                # # print(inventory,'location in stock')
-                #
-                #
                 re_order=self.env['stock.warehouse.orderpoint'].search([('location_id','=',l.id),('product_id','=',self.product_db_id.id)])
                 for ro in re_order:
                     if ro:
                         reorder_qty=str(ro.product_min_qty)
                         reorder_level=str(ro.product_max_qty)
-                # for inv in inventory:
-                #     print(inv.id,'inventory')
-                #     print(inv.product_id.name,'product')
-                #
                     if  q.quantity>0 and l.id not in loations_stock:
                         loations_stock.append(l.id)
-                #     quantities+=inv.quantity
-
             data_all.append({
                 'branch':w.id,
                 'loc_ids':[(6,0,loations_stock)],
@@ -294,7 +287,156 @@ class CustomDashboard(models.TransientModel):
         else:
             raise UserError("Select Product First")
 
+    def action_refresh(self):
 
+        def add_log_ids_dash():
+            logs = self.env['dasboard.logs'].search([('product_id', '=', self.product_db_id.id)])
+            self.dashboard_logs_ids = logs
+
+        add_log_id = fields.Many2many(comodel_name='add.log')
+
+
+
+        def get_warehoueinfo(self):
+            warehoues = self.env['stock.warehouse'].search([])
+            locations = []
+
+            def get_locations(childs):
+                locations_childs = self.env['stock.location']
+                for c in childs:
+                    if c.child_ids:
+                        ans = get_locations(c.child_ids)
+                        for val in ans:
+                            locations_childs += val
+                        locations_childs += c
+                    else:
+                        locations_childs += c
+                return locations_childs
+
+            data_all = []
+            for w in warehoues:
+
+                quantities = 0
+                locations = get_locations(w.view_location_id)
+
+                loations_stock = []
+                price_list = self.env['product.pricelist'].search([('warehouse_id', '=', w.id)])
+                price_list_item = self.env['product.pricelist.item'].search(
+                    [('pricelist_id', '=', price_list.id), ('product_id', '=', self.product_db_id.id)])
+                cost = self.env['product.product'].search([('id', '=', self.product_db_id.id)])
+
+                reorder_qty = "Not Maintanied"
+                reorder_level = "Not Maintanied"
+                for l in locations:
+                    for q in l.quant_ids:
+                        if q.product_id == self.product_db_id:
+                            quantities += q.available_quantity
+                    re_order = self.env['stock.warehouse.orderpoint'].search(
+                        [('location_id', '=', l.id), ('product_id', '=', self.product_db_id.id)])
+                    for ro in re_order:
+                        if ro:
+                            reorder_qty = str(ro.product_min_qty)
+                            reorder_level = str(ro.product_max_qty)
+
+                        if q.quantity > 0 and l.id not in loations_stock:
+                            loations_stock.append(l.id)
+
+
+                data_all.append({
+                    'branch': w.id,
+                    'loc_ids': [(6, 0, loations_stock)],
+                    'qty_inhand': quantities,
+                    'max_price': price_list_item.fixed_price,
+                    'min_price': price_list_item.min_price,
+                    'trader': price_list_item.trade_price,
+                    'export_price': price_list_item.export_price,
+                    'cost': cost.standard_price,
+                    'reorder_qty': reorder_qty,
+                    'reorder_level': reorder_level,
+                    'warehouse_shop': w.ware_type
+                })
+
+            enter_data_shop = self.env['dashboard.shop']
+            enter_data_warehouse = self.env['dashboard.warehouseshop']
+
+            for da in data_all:
+                if da['warehouse_shop'] == "shop":
+                    dev_lines1 = self.env['dashboard.shop'].create(da)
+                    enter_data_shop += dev_lines1
+
+                elif da['warehouse_shop'] == "warehouse":
+                    dev_lines = self.env['dashboard.warehouseshop'].create(da)
+                    enter_data_warehouse += dev_lines
+            self.db_shop_ids = enter_data_shop
+            self.db_warehouse_ids = enter_data_warehouse
+
+
+            def _get_quotation_history():
+                sale_line_q = self.env['sale.order.line'].search([("product_id", "=", self.product_db_id.id)])
+                sa_ids = []
+                so_ids = []
+                for line in sale_line_q:
+                    if line.order_id.state == 'draft':
+                        sa_ids.append(line.id)
+                    elif line.order_id.state == 'sale':
+                        so_ids.append(line.id)
+                self.quotation_his_id = sa_ids
+                self.customer_order_ids = so_ids
+
+            def purchase_lines():
+                purchase_line_q = self.env['purchase.order.line'].search([("product_id", "=", self.product_db_id.id)])
+                po_ids = []
+                lpo_ids = []
+                lporfo = []
+                porfo = []
+                for line in purchase_line_q:
+                    if line.order_id.state == 'purchase' or line.order_id.state == 'done':
+                        if line.order_id.order_type == 'purchase_order':
+                            po_ids.append(line.id)
+                        elif line.order_id.order_type == 'lpo':
+                            lpo_ids.append(line.id)
+                    if line.order_id.state == 'draft':
+                        if line.order_id.order_type == 'purchase_order':
+                            porfo.append(line.id)
+                        elif line.order_id.order_type == 'lpo':
+                            lporfo.append(line.id)
+
+                self.purchase = po_ids
+                self.local_puchase = lpo_ids
+                self.rfq_puchase = porfo
+                self.rfq_local_puchase = lporfo
+
+            def get_supplier():
+                po = self.env['purchase.order.line'].search([('product_id', '=', self.product_db_id.id)],
+                                                            order='id asc')
+                self.last_supplier = False
+                self.supplier_reference = ""
+                self.last_purchase_cost = 0
+                self.avg_purchase_cost = 0
+                cost_sum = 0
+                for pol in po:
+                    if pol.order_id.state == 'purchase' or pol.order_id.state == 'done':
+                        self.last_supplier = pol.partner_id
+                        self.supplier_reference = pol.order_id.partner_ref
+                        self.last_purchase_cost = pol.price_unit
+                        cost_sum += pol.price_unit
+                if po.ids:
+                    self.avg_purchase_cost = cost_sum / len(po.ids)
+
+            def addlogs():
+
+                if self.product_db_id:
+                    self.write({
+                        "dashboard_logs_ids": [(0, 0, {
+                            'product_id': self.product_db_id.id,
+                            'description': self.logs_description,
+                            'user_id': self.env.user.id,
+                            'price': self.logs_price,
+                            'partner_id': self.logs_partner_id.id if self.logs_partner_id else False
+                        })]
+                    })
+                else:
+                    raise UserError("Select Product First")
 
 
 
